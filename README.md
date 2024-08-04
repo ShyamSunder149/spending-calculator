83c1d387ab2ff84ee5695091b4acd4da111327c0
ShyamSunder149 <SubstantialCitron0@protonmail.com>
2024-08-04 12:31:59 +0530

    fixed stuff, flag implementation and export feature

--- README.md
+++ README.md
@@ -3,11 +3,11 @@
 This is basically one of my needs to do a finance manager and I don't want to make an entry daily on what I spend and do. And I find myself lazy to code an app which can scrape the messages which comes to the phone and shows you stuff. So instead I used google takeout to export my activities in google pay as html file and updated it as input to my program so I can check the spendings every month end. Please feel free to check out and let me know your suggestions. TIA
 
 #### To-do :
-- [ ] fix category expenses  
+- [x] fix category expenses  
 - [x] Pretty print output
 - [x] Table display
-- [ ] Export to csv option
-- [ ] flags implementation
+- [x] Export to csv option
+- [x] flags implementation
 - [ ] Average per month spending calculation 
 - [ ] Calculate big expenses separately
 - [ ] Categorise big expenses 
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+import argparse
 import copy
 from prettytable import PrettyTable 
 
@@ -7,14 +8,30 @@ class Expense:
     def __init__(self):
         self.total_debit = 0
         self.total_credit = 0
-        self.category_spendings = {
-            "travel" : 0,
-            "entertainment" : 0,
-            "stocks" : 0,
-            "online_purchase" : 0
-        }
+        self.category_spendings = {}
+
+def check_category(category : str, expense : Expense) -> int : 
+    if category in expense.category_spendings.keys() :
+        return expense.category_spendings[category]
+    return 0
+
+def check_add(options : list[str], text : str, expense : Expense, category : str, amount : int) :
+    for i in options :
+        if i in text :
+            if category in expense.category_spendings.keys() :
+                expense.category_spendings[category] += amount
+            else :
+                expense.category_spendings[category] = amount
 
 def main() :
+
+    parser = argparse.ArgumentParser(description="spending tracker for google pay",
+                                     epilog="Hope that helps!!")
+    parser.add_argument('-ecsv',
+                        help= "export contents as csv",
+                        action="store_true")
+    args = parser.parse_args()
+
     f = open("takeout.html", "r").read()
     parsed_html = BeautifulSoup(f, features="lxml")
     divs = parsed_html.body.find_all("div", attrs={'class' : 'content-cell'})
@@ -33,7 +50,7 @@ def main() :
     rs = {"debit" : 0, "credit" : 0}
     months = "Jan,Feb,Mar,Apr,May,Jun,Jul,Aug,Sep,Oct,Nov,Dec".split(",")
     for i in months :
-        monthly_spendings[i] = copy.copy(expense)
+        monthly_spendings[i] = copy.deepcopy(expense)
     
     for i in range(len(money_divs)) :
         if "Completed" in transaction_divs[i].text :
@@ -46,28 +63,28 @@ def main() :
                     monthly_spendings[month].total_credit += amount 
                 else :
                     monthly_spendings[month].total_debit += amount
-                for i in split_comps:
-                    if i in travel :
-                        monthly_spendings[month].category_spendings["travel"] += amount 
-                    elif i in entertainment :
-                        monthly_spendings[month].category_spendings["entertainment"] += amount 
-                    if i in stocks : 
-                        monthly_spendings[month].category_spendings["stocks"] += amount 
-                    elif i in online_purchase :
-                        monthly_spendings[month].category_spendings["online_purchase"] += amount 
-   
+
+                check_add(travel, money_divs[i].text, monthly_spendings[month], "travel", amount)
+                check_add(entertainment, money_divs[i].text, monthly_spendings[month], "entertainment", amount)
+                check_add(stocks, money_divs[i].text, monthly_spendings[month], "stocks", amount)
+                check_add(online_purchase, money_divs[i].text, monthly_spendings[month], "online_purchase", amount)
+
     table = PrettyTable()
     table.field_names = ["Month", "Debit", "Credit", "Net", "Travel", "Entertainment", "Online Purchase", "Stocks"]
-    for i in months:
+    for i in months: 
         table.add_row([i,
                       monthly_spendings[i].total_debit, 
                       monthly_spendings[i].total_credit, 
                       monthly_spendings[i].total_debit-monthly_spendings[i].total_credit, 
-                      monthly_spendings[i].category_spendings["travel"], 
-                      monthly_spendings[i].category_spendings["entertainment"], 
-                      monthly_spendings[i].category_spendings["online_purchase"],
-                      monthly_spendings[i].category_spendings["stocks"]])
+                      check_category("travel", monthly_spendings[i]), 
+                      check_category("entertainment", monthly_spendings[i]), 
+                      check_category("online_purchase", monthly_spendings[i]),
+                      check_category("stocks", monthly_spendings[i])])
     print(table)
+
+    if args.ecsv :
+        with open('test.csv', 'w', newline='') as f_output:
+            f_output.write(table.get_csv_string())
 
 if __name__ == "__main__" :
     main()
